2efe69434a5c08dacf39989019efceeb4e181cd0
05Arda <arda_ozturk_05@outlook.com>
2025-12-07 14:29:59 +0300

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 // app/layout.js
-import { Inter } from "next/font/google";
 import "./globals.css";
+import { Inter } from "next/font/google";
 import { Providers } from "@/components/providers"; // Yeni oluşturduğumuz provider
 
 const inter = Inter({ subsets: ["latin"] });
