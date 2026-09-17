7f50bc789816257fd64841c19a4de57bc4aa7739
Arda Öztürk <66124224+05Arda@users.noreply.github.com>
2026-09-17 21:58:09 +0300

    Changed logo on browser

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,16 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Trace",
   description: "Sui Blockchain Event Platform",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/icon.png", type: "image/png", sizes: "32x32" },
+      { url: "/tracelogo.svg", type: "image/svg+xml" },
+    ],
+    apple: [
+      { url: "/apple-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
